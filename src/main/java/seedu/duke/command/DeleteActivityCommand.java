856f4d6c28e97f1089f48704be9a19315af102a1
JellybeanJoel <e1398423@u.nus.edu>
2026-03-15 16:51:19 +0800

    Added DeleteActivity and EditActivity classes

---
+++ src/main/java/seedu/duke/command/DeleteActivityCommand.java
@@ -0,0 +1,25 @@
+package seedu.duke.command;
+
+import seedu.duke.model.Activity;
+import seedu.duke.model.ActivityList;
+
+public class DeleteActivityCommand extends ActivityCommand {
+    private final int index;
+
+    public DeleteActivityCommand(ActivityList activityList, int index) {
+        super(activityList);
+        this.index = index;
+    }
+
+    public String execute(String tripName) {
+        int zeroBasedIndex = index - 1;
+
+        if (zeroBasedIndex < 0 || zeroBasedIndex >= activityList.size()) {
+            return "Oops! Invalid activity index provided.";
+        }
+
+        Activity removedActivity = activityList.remove(zeroBasedIndex);
+
+        return "Activity deleted:\n\n" + removedActivity.getName() + "\n";
+    }
+}
