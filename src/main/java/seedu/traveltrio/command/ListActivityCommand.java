962a5d17878ac237ad64593c891d2080cad7be1c
5iu5 <e1384442@u.nus.edu>
2026-03-19 15:52:53 +0800

    Add ListActivityCommand class

---
+++ src/main/java/seedu/traveltrio/command/ListActivityCommand.java
@@ -0,0 +1,25 @@
+package seedu.traveltrio.command;
+
+import java.util.ArrayList;
+import seedu.traveltrio.model.Activity;
+import seedu.traveltrio.model.ActivityList;
+
+public class ListActivityCommand extends ActivityCommand {
+
+    public ListActivityCommand(ActivityList activityList){
+        super(activityList);
+    }
+
+    public String execute(String tripName){
+        if (activityList.isEmpty()){
+            return "The itinerary is Empty.";
+        }
+
+        String listToPrint = "Itinerary for " + tripName + ":\n\n";
+        for (int i = 0; i < activityList.size(); i++){
+            listToPrint += ((i+1) + ". " + activityList.get(i).formatForList() + "\n\n");
+        }
+        return listToPrint;
+
+    }
+}
