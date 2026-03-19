bf95c1aa8d61ab068495e83298f20dc672e75219
Weng Xian Yang <e1398191@u.nus.edu>
2026-03-19 13:49:21 +0800

    Add TripCommand, OpenTrip, DeleteTrip
    Edited ActivityCommand to open trip

---
+++ src/main/java/seedu/duke/command/ActivityCommand.java
@@ -0,0 +1,25 @@
+package seedu.duke.command;
+
+import seedu.duke.model.ActivityList;
+
+public abstract class ActivityCommand {
+
+    protected ActivityList activityList;
+
+    private static boolean isTripOpen = false;
+
+    public ActivityCommand(ActivityList activityList) {
+        this.activityList = activityList;
+
+        if (!isTripOpen) {
+            throw new IllegalStateException("No trip is currently open. Please open a trip to manage activities.");
+        }
+    }
+
+    public static void setTripOpen(boolean isOpen) {
+        isTripOpen = isOpen;
+    }
+
+    public abstract String execute(String tripName);
+
+}
