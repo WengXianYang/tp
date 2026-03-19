a08ec4e46e3e037bda2129239f2d5b020897e239
JellybeanJoel <e1398423@u.nus.edu>
2026-03-19 16:20:38 +0800

    Added EditActivityCommand and DeleteActivityCommand

---
+++ src/main/java/seedu/traveltrio/command/DeleteActivityCommand.java
@@ -0,0 +1,27 @@
+package seedu.traveltrio.command;
+
+
+import seedu.traveltrio.model.Activity;
+import seedu.traveltrio.model.ActivityList;
+
+public class DeleteActivityCommand extends ActivityCommand {
+    private final int index;
+
+    public DeleteActivityCommand(ActivityList activityList, int index) {
+        super(activityList);
+        this.index = index;
+    }
+
+    public String execute(String tripName) {
+        int zeroBasedIndex = index - 1;
+
+        if (zeroBasedIndex < 0 || zeroBasedIndex >= activityList.size()) {
+            return "Oops! Invalid activity index provided.";
+        }
+
+        Activity removedActivity = activityList.remove(zeroBasedIndex);
+
+        return "Activity deleted:\n\n" + removedActivity.getName() + "\n";
+    }
+}
+
